d922ca5f5f2ddc7530d24e5700e5ce69e45b57e8
Erick Mora <m.erickpeligro@gmail.com>
2025-07-23 18:34:39 -0600

    migrations

--- app/Models/Movies.php
+++ app/Models/Movies.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movies extends Model
 {
-    protected $fillable = ['title', 'description', 'comments', 'fav_ost', 'final_note'];
+     protected $fillable = ['title', 'description', 'comments', 'fav_ost', 'final_note'];
 }
